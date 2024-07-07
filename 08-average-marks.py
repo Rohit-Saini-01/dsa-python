ec2621dbd9de96c878594973be5a6b4665f2a3cd
Rohit Saini <emailrohitsaini@gmail.com>
2024-07-08 02:50:07 +0530

    average marks

---
+++ 08-average-marks.py
@@ -0,0 +1,7 @@
+marks_1 = int(input("Enter marks of student 1:"))
+marks_2 = int(input("Enter marks of student 2:"))
+marks_3 = int(input("Enter marks of student 3:"))
+
+avg_marks = (marks_1 + marks_2 + marks_3) / 3
+
+print(avg_marks)
