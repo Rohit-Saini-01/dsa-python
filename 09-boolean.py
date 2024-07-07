f1ef1a82a42bfc7287d2c6faad40513b5aed45e2
Rohit Saini <emailrohitsaini@gmail.com>
2024-07-08 03:14:54 +0530

    boolean, rlational and logical operators

---
+++ 09-boolean.py
@@ -0,0 +1,18 @@
+var_1 = True  # Boolean values
+var_2 = False
+
+num_1 = 10
+num_2 = 12
+
+# Relational operators
+print(num_1 >= num_2)
+print(num_1 <= num_2)
+print(num_1 > num_2)
+print(num_1 < num_2)
+print(num_1 == num_2)
+print(num_1 != num_2)
+
+# Logical operators
+print(var_1 and var_2)
+print(var_1 or var_2)
+print(not var_2)
