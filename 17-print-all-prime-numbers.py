3cbede85ac7974bac85a74db1813928ac08db378
Rohit Saini <emailrohitsaini@gmail.com>
2024-07-14 11:41:29 +0530

    improved code

--- 17-print-all-prime-numbers.py
+++ 17-print-all-prime-numbers.py
@@ -1,14 +1,14 @@
 num = int(input("Enter a number:"))
 count = 2
-flag = False
 
 while count <= num:
     count_2 = 2
+    flag = False
+
     while count_2 < count:
         if count % count_2 == 0:
             flag = True
         count_2 = count_2 + 1
     if not (flag):
         print(count)
-    flag = False
     count = count + 1
