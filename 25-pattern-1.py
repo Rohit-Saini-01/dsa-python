3a70408b97acfd7d07549c66543966445ea6e8e3
Rohit Saini <emailrohitsaini@gmail.com>
2024-07-18 14:55:52 +0530

    changed naming of variables

--- 25-pattern-1.py
+++ 25-pattern-1.py
@@ -1,9 +1,9 @@
 num = int(input("Enter the value of n:"))
-i = 1
-while i <= num:
-    j = 1
-    while j <= num:
+row = 1
+while row <= num:
+    col = 1
+    while col <= num:
         print("*", end="")
-        j += 1
+        col += 1
     print()
-    i += 1
+    row += 1
