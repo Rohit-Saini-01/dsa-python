671eacabfe0baa77020a7f63b4bdb0058555f601
Rohit Saini <emailrohitsaini@gmail.com>
2024-07-18 19:35:49 +0530

    character pattern

--- 34-character-pattern.py
+++ 34-character-pattern.py
@@ -1,11 +1,11 @@
 num = int(input("Enter the number of rows you wanna print:"))
 
 row = 1
-
 while row <= num:
+    start_char = chr(ord("A") + row - 1)
     col = 1
     while col <= num:
-        print(chr(ord("A") + col - 1), end="")
+        print(chr(ord(start_char) + col - 1), end="")
         col += 1
     print()
     row += 1
