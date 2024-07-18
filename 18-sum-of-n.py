78b350d29c1a43455deee80e0ef0c190e94f74bb
Rohit Saini <emailrohitsaini@gmail.com>
2024-07-18 12:12:34 +0530

    while loop

---
+++ 18-sum-of-n.py
@@ -0,0 +1,9 @@
+num = int(input("Enter the value of num:"))
+
+count = 1
+sum = 0
+while count <= num:
+    sum += count
+    count = count + 1
+
+print(sum)
---
+++ 19-sum-of-even-num.py
@@ -0,0 +1,7 @@
+num = int(input("Enter the value of num:"))
+count = 0
+sum = 0
+while count <= num:
+    sum += count
+    count = count + 2
+print(sum)
