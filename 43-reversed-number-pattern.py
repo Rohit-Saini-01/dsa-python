a2ac76e4b0ee1b7cacc12167d93ddc9108d0b677
Rohit Saini <emailrohitsaini@gmail.com>
2024-07-19 08:57:49 +0530

    reversed number pattern

--- 43-reversed-number-pattern.py
+++ 43-reversed-number-pattern.py
@@ -1,16 +1,13 @@
-num = int(input("Enter the value of num:"))
-
+num = int(input("Enter the value of n:"))
 row = 1
-
 while row <= num:
     col = 1
     while col <= num - row:
         print(" ", end="")
         col += 1
-
     col = 1
     while col <= row:
-        print("*", end="")
+        print(col, end="")
         col += 1
-    print()
     row += 1
+    print()
